00adcf57cbbed6c83b2d704d875e6c7a0f8249b8
Marius Karlsen <mariusskarlsen@gmail.com>
2022-02-08 14:23:34 +0000

    automatic float switch

--- commands/server.js
+++ commands/server.js
@@ -1,11 +1,14 @@
 module.exports = {
   name: "server",
-  description: "server test",
+  description: "Enables server automation and status checking",
   async execute(client, message, args) {
     const { spawn } = require("child_process");
     const { MessageEmbed } = require("discord.js");
 
     // ping -i 1 -c 4 192.168.0.105
+
+    // Keeping track if the floating ip is set to the balancer or not.
+    let ipBalancer = true;
 
     setInterval(() => {
       const balancer = spawn("ping", ["-c 1", "192.168.131.142"]);
@@ -13,78 +16,98 @@ module.exports = {
       const www2 = spawn("ping", ["-c 1", "192.168.132.34"]);
       const docserv = spawn("ping", ["-c 1", "192.168.130.3"]);
       const db = spawn("ping", ["-c 1", "192.168.128.166"]);
+      const test = spawn("ping", ["-c 1", "192.168.128.44"]);
 
-      const servers = [balancer, www1, www2, docserv, db];
-
-      // const servers = [docserv, db];
-
-      // const names = ["docserv", "db"];
-
-      const names = ["Balancer", "WWW1", "WWW2", "Docker", "DB"];
+      const servers = [balancer, www1, www2, docserv, db, test];
+      const names = ["Balancer", "WWW1", "WWW2", "Docker", "DB", "Test"];
 
       let scriptOutput = "";
 
+      // Looping through the servers and checking status
       for (const server of servers) {
+        // Normal
         server.stdout.setEncoding("utf8");
         server.stdout.on("data", async (data) => {
-          // console.log(`stdout: ${data} hellohello`);
           data = data.toString();
           data;
         });
-
+        //error
         server.stderr.on("data", async (data) => {
           console.error(`stderr: ${data}`);
           data = data.toString();
-          scriptoutput += data;
+          // scriptoutput += data;
         });
 
+        // Exit child
         server.on("close", async (code) => {
           await console.log(`child process exited with code ${code}`);
 
+          // if error
           if (code != 0) {
+            // if the balancer is down, try to switch to the emergency temp server
+            if (
+              names[servers.indexOf(server)].toString() == "Balancer" &&
+              ipBalancer == true
+            ) {
+              try {
+                await message.channel.send("Trying to switch test...");
+                await spawn("downtime");
+                await message.channel.send("Switched to test!");
+                ipBalancer = false;
+              } catch (e) {
+                await message.channel.send(e);
+              }
+            }
+
             await message.reply(
               `${names[servers.indexOf(server)].toString()} is DOWN!!  📉 `
             );
-          } else {
-            // await message.reply(
-            //   `${names[servers.indexOf(server)].toString()} is up! ✅ `
-            // );
+          } else if (
+            ipBalancer == false &&
+            names[servers.indexOf(server)].toString() == "Balancer"
+          ) {
+            try {
+              await message.channel.send(
+                "Trying to switch back to Balancer..."
+              );
+              await spawn("downtimeFix");
+              ipBalancer = true;
+              await message.channel.send("Switched back to balancer!");
+            } catch (e) {
+              await message.channel.send(e);
+            }
           }
-
-          // if (scriptOutput.includes("0 received")) {
-          //   await message.reply(
-          //     `${names[servers.indexOf(server)].toString()} is DOWN!!  📉 `
-          //   );
-          // }
         });
       }
-
-      // balancer.stdout.setEncoding("utf8");
-      // balancer.stdout.on("data", async (data) => {
-      //   // console.log(`stdout: ${data} hellohello`);
-      //   data = data.toString();
-      //   scriptOutput += data;
-      // });
-
-      // balancer.stderr.on("data", async (data) => {
-      //   console.error(`stderr: ${data}`);
-      //   data = data.toString();
-      //   scriptOutput += data;
-      // });
-
-      // balancer.on("close", async (code) => {
-      //   await console.log(`child process exited with code ${code}`);
-
-      //   await console.log(scriptOutput, "from output");
-
-      //   if (scriptOutput.includes("0 received")) {
-      //     await message.reply(scriptOutput);
-      //   } else {
-      //     await message.reply("Balancer is up! ✅ ");
-      //   }
-      // });
-    }, 300000);
+    }, 1800000);
 
     await message.reply("Started listening!");
   },
 };
+
+// Template
+
+// balancer.stdout.setEncoding("utf8");
+// balancer.stdout.on("data", async (data) => {
+//   // console.log(`stdout: ${data} hellohello`);
+//   data = data.toString();
+//   scriptOutput += data;
+// });
+
+// balancer.stderr.on("data", async (data) => {
+//   console.error(`stderr: ${data}`);
+//   data = data.toString();
+//   scriptOutput += data;
+// });
+
+// balancer.on("close", async (code) => {
+//   await console.log(`child process exited with code ${code}`);
+
+//   await console.log(scriptOutput, "from output");
+
+//   if (scriptOutput.includes("0 received")) {
+//     await message.reply(scriptOutput);
+//   } else {
+//     await message.reply("Balancer is up! ✅ ");
+//   }
+// });
--- commands/status.js
+++ commands/status.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: "status",
-  description: "server test",
+  description: "Gets a graphical status of the server",
   async execute(client, message, args) {
     const { spawn } = require("child_process");
     const { MessageEmbed } = require("discord.js");
@@ -10,8 +10,9 @@ module.exports = {
     const www2 = spawn("ping", ["-c 1", "192.168.132.34"]);
     const docserv = spawn("ping", ["-c 1", "192.168.130.3"]);
     const db = spawn("ping", ["-c 1", "192.168.128.166"]);
+    const test = spawn("ping", ["-c 1", "192.168.128.44"]);
 
-    const servers = [balancer, www1, www2, docserv, db];
+    const servers = [balancer, www1, www2, docserv, db, test];
 
     const EmbedFiles = new MessageEmbed()
       .setColor("#b98f01")
@@ -23,7 +24,7 @@ module.exports = {
 
     // const names = ["docserv", "db"];
 
-    const names = ["Balancer", "WWW1", "WWW2", "Docker", "DB"];
+    const names = ["Balancer", "WWW1", "WWW2", "Docker", "DB", "Test"];
 
     let field = "";
 
@@ -42,7 +43,7 @@ module.exports = {
       server.stderr.on("data", (data) => {
         console.error(`stderr: ${data}`);
         data = data.toString();
-        scriptoutput += data;
+        // scriptoutput += data;
       });
 
       server.on("close", async (code) => {
